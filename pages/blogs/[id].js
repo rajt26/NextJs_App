eb37c4dbd477c998ec08b79425065090ef00d923
Raj Thakkar <raj@logicwind.com>
2023-01-04 14:47:40 +0530

    changes

--- pages/blogs/[id].js
+++ pages/blogs/[id].js
@@ -1,10 +1,7 @@
 import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
-import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
-import Link from 'next/link';
-import Layout from '../../layout/layout';
+
 export const getStaticPaths = async () => {
     const res = await fetch('https://jsonplaceholder.typicode.com/posts');
     const data = await res.json();
